32e655a20b29f5d8170034320a66c7cbc9fe1167
ReyXX777 <cse2021100@rcciit.org.in>
2024-11-28 01:54:02 +0530

    Update admin.py
    raised performance

--- backend/kafka_support_backend/core/migrations/admin.py
+++ backend/kafka_support_backend/core/migrations/admin.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
-from .models import (
-    YourModel1,
-    YourModel2,
-    YourModel3
-)  # Import your models here
-
-# Define custom admin classes for better control over the admin interface
+from .models import YourModel1, YourModel2, YourModel3
 
 class YourModel1Admin(admin.ModelAdmin):
-    list_display = ('field1', 'field2', 'field3')  # Fields to display in the list view
-    search_fields = ('field1', 'field2')  # Fields to be searchable
-    list_filter = ('field3',)  # Fields to filter by in the admin
+    list_display = ('field1', 'field2', 'field3')
+    search_fields = ('field1', 'field2')
+    list_filter = ('field3',)
 
 class YourModel2Admin(admin.ModelAdmin):
     list_display = ('name', 'created_at', 'is_active')
@@ -22,9 +16,6 @@ class YourModel3Admin(admin.ModelAdmin):
     search_fields = ('title',)
     list_filter = ('status',)
 
-# Register your models with the admin site
 admin.site.register(YourModel1, YourModel1Admin)
 admin.site.register(YourModel2, YourModel2Admin)
 admin.site.register(YourModel3, YourModel3Admin)
-
-# If you have more models, continue to add them in a similar fashion
