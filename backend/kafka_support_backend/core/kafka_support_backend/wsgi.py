ae15e9025b2d64ccc6d94794fc5447b572ddaf3c
ReyXX777 <cse2021100@rcciit.org.in>
2024-12-27 22:56:16 +0530

    Update wsgi.py
    import required modules

--- backend/kafka_support_backend/core/kafka_support_backend/wsgi.py
+++ backend/kafka_support_backend/core/kafka_support_backend/wsgi.py
@@ -1,6 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Set the default Django settings module for the WSGI application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kafka_support_backend.settings")
 
+# Create the WSGI application object
 application = get_wsgi_application()
