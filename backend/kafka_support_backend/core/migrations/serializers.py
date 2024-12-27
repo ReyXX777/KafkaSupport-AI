64bc0d9a795cc29a9bd6cfcf083fba1bc3f86686
ReyXX777 <cse2021100@rcciit.org.in>
2024-12-27 23:01:59 +0530

    Update serializers.py
    meta class

--- backend/kafka_support_backend/core/migrations/serializers.py
+++ backend/kafka_support_backend/core/migrations/serializers.py
@@ -2,21 +2,33 @@ from rest_framework import serializers
 from .models import KafkaCluster, Topic, ConsumerGroup, Broker
 
 class KafkaClusterSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the KafkaCluster model.
+    """
     class Meta:
         model = KafkaCluster
-        fields = '__all__'
+        fields = '__all__'  # Include all fields from the model
 
 class TopicSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Topic model.
+    """
     class Meta:
         model = Topic
-        fields = '__all__'
+        fields = '__all__'  # Include all fields from the model
 
 class ConsumerGroupSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the ConsumerGroup model.
+    """
     class Meta:
         model = ConsumerGroup
-        fields = '__all__'
+        fields = '__all__'  # Include all fields from the model
 
 class BrokerSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Broker model.
+    """
     class Meta:
         model = Broker
-        fields = '__all__'
+        fields = '__all__'  # Include all fields from the model
