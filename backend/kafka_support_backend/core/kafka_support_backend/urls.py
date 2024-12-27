852fbc92ba890db1c6859ea6a70aa2f731f25942
ReyXX777 <cse2021100@rcciit.org.in>
2024-12-27 22:55:18 +0530

    Update urls.py
    admin interface

--- backend/kafka_support_backend/core/kafka_support_backend/urls.py
+++ backend/kafka_support_backend/core/kafka_support_backend/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Admin interface URL
     path("admin/", admin.site.urls),
+
+    # API routes for the "activity" app
     path("api/activity/", include("activity.urls")),
 ]
