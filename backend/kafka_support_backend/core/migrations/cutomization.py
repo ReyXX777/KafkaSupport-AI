aca1704cf67614b3e98270a9acfcdc98ee1d3c4f
ReyXX777 <cse2021100@rcciit.org.in>
2024-12-27 23:00:08 +0530

    Update cutomization.py
    registering modules

--- backend/kafka_support_backend/core/migrations/cutomization.py
+++ backend/kafka_support_backend/core/migrations/cutomization.py
@@ -1,11 +1,50 @@
 from django.contrib import admin
 from .models import KafkaCluster, Topic, ConsumerGroup, Broker
 
+# Custom admin class for KafkaCluster
 @admin.register(KafkaCluster)
 class KafkaClusterAdmin(admin.ModelAdmin):
-    list_display = ('name', 'version', 'location') 
-    list_filter = ('location',) 
-    search_fields = ('name',) 
+    # Fields to display in the list view
+    list_display = ('name', 'version', 'location')
+    # Fields to enable filtering
+    list_filter = ('location',)
+    # Fields to enable search functionality
+    search_fields = ('name',)
+    # Default ordering of records
     ordering = ('name',)
 
-# Similar customizations can be done for the other models.
+# Custom admin class for Topic
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    # Fields to display in the list view
+    list_display = ('name', 'cluster', 'partitions', 'replication_factor')
+    # Fields to enable filtering
+    list_filter = ('cluster',)
+    # Fields to enable search functionality
+    search_fields = ('name',)
+    # Default ordering of records
+    ordering = ('name',)
+
+# Custom admin class for ConsumerGroup
+@admin.register(ConsumerGroup)
+class ConsumerGroupAdmin(admin.ModelAdmin):
+    # Fields to display in the list view
+    list_display = ('name', 'topic', 'offset')
+    # Fields to enable filtering
+    list_filter = ('topic',)
+    # Fields to enable search functionality
+    search_fields = ('name',)
+    # Default ordering of records
+    ordering = ('name',)
+
+# Custom admin class for Broker
+@admin.register(Broker)
+class BrokerAdmin(admin.ModelAdmin):
+    # Fields to display in the list view
+    list_display = ('host', 'port', 'cluster')
+    # Fields to enable filtering
+    list_filter = ('cluster',)
+    # Fields to enable search functionality
+    search_fields = ('host',)
+    # Default ordering of records
+    ordering = ('host',)
