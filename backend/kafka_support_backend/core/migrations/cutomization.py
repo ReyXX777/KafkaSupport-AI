0427e4a77e3940650b8650292b794a447cc2ee67
AzadXx77 <cse2021100@rcciit.org.in>
2025-01-17 23:54:29 +0530

    Update cutomization.py
    Added export to CSV and custom admin actions for Kafka models

--- backend/kafka_support_backend/core/migrations/cutomization.py
+++ backend/kafka_support_backend/core/migrations/cutomization.py
@@ -1,5 +1,9 @@
+# commit message: Added export to CSV and custom admin actions for Kafka models
+
 from django.contrib import admin
 from .models import KafkaCluster, Topic, ConsumerGroup, Broker
+from django.http import HttpResponse
+import csv
 
 # Custom admin class for KafkaCluster
 @admin.register(KafkaCluster)
@@ -13,6 +17,22 @@ class KafkaClusterAdmin(admin.ModelAdmin):
     # Default ordering of records
     ordering = ('name',)
 
+    # Add export to CSV action
+    actions = ['export_to_csv']
+
+    def export_to_csv(self, request, queryset):
+        """
+        Custom admin action to export selected Kafka clusters to CSV.
+        """
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="kafka_clusters_export.csv"'
+        writer = csv.writer(response)
+        writer.writerow(['Name', 'Version', 'Location'])  # CSV headers
+        for obj in queryset:
+            writer.writerow([obj.name, obj.version, obj.location])
+        return response
+    export_to_csv.short_description = "Export selected clusters to CSV"
+
 # Custom admin class for Topic
 @admin.register(Topic)
 class TopicAdmin(admin.ModelAdmin):
@@ -24,6 +44,17 @@ class TopicAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     # Default ordering of records
     ordering = ('name',)
+
+    # Add custom admin action to reset partitions
+    actions = ['reset_partitions']
+
+    def reset_partitions(self, request, queryset):
+        """
+        Custom admin action to reset partitions for selected topics.
+        """
+        queryset.update(partitions=1)
+        self.message_user(request, f"Reset partitions for {queryset.count()} topics.")
+    reset_partitions.short_description = "Reset partitions to 1"
 
 # Custom admin class for ConsumerGroup
 @admin.register(ConsumerGroup)
@@ -37,6 +68,17 @@ class ConsumerGroupAdmin(admin.ModelAdmin):
     # Default ordering of records
     ordering = ('name',)
 
+    # Add custom admin action to reset offsets
+    actions = ['reset_offsets']
+
+    def reset_offsets(self, request, queryset):
+        """
+        Custom admin action to reset offsets for selected consumer groups.
+        """
+        queryset.update(offset=0)
+        self.message_user(request, f"Reset offsets for {queryset.count()} consumer groups.")
+    reset_offsets.short_description = "Reset offsets to 0"
+
 # Custom admin class for Broker
 @admin.register(Broker)
 class BrokerAdmin(admin.ModelAdmin):
@@ -48,3 +90,14 @@ class BrokerAdmin(admin.ModelAdmin):
     search_fields = ('host',)
     # Default ordering of records
     ordering = ('host',)
+
+    # Add custom admin action to deactivate brokers
+    actions = ['deactivate_brokers']
+
+    def deactivate_brokers(self, request, queryset):
+        """
+        Custom admin action to mark selected brokers as inactive.
+        """
+        queryset.update(is_active=False)
+        self.message_user(request, f"Deactivated {queryset.count()} brokers.")
+    deactivate_brokers.short_description = "Deactivate selected brokers"
